80e2fb0c25c37fabe06ee66b294a42e268f3b6b5
OliverRissato <oliverrissato@usp.br>
2025-01-19 19:33:25 -0300

    Creating some of input logic and clock frame

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     createPhrase();
 
+    let actualTime = 0;
+
+    resetTime();
+
     function getPhrase() {
         const phrase = new String("Frase do dia");
 
@@ -25,4 +29,35 @@ document.addEventListener("DOMContentLoaded", () => {
             par.appendChild(letter);
           }
     }
+
+    function resetTime() {
+        actualTime = 0;
+
+        const time_output = document.getElementById("actual-time");
+
+        time_output.innerHTML = "Time: " + String(actualTime) + "s";
+    }
+
+    const inputText = document.getElementById('type-text');
+
+    inputText.addEventListener("input", (event) => {
+        var inputValue = event.target.value;
+
+        const phrase = getPhrase();
+
+        for (let index = 0; index < phrase.length; index++)
+        {
+            var letter = document.getElementById(index+1);
+
+            if(letter.innerHTML == inputValue[index])
+            {
+                letter.className = "typed";
+            }
+            else
+            {
+                letter.className = "untyped";
+            }
+        }
+    });
 })
+
